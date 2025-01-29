cf2a9f5b5bd0d83277084527eaae40d2b17bd0f6
Rodrigo Salinas <rodrigo.salinas@catena.cl>
2025-01-29 15:59:25 -0300

    init

---
+++ parvuloapi/src/main/java/cl/mineduc/sidep/parvuloapi/ParvuloapiApplication.java
@@ -0,0 +1,13 @@
+package cl.mineduc.sidep.parvuloapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParvuloapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParvuloapiApplication.class, args);
+    }
+
+}
---
+++ parvuloapi/src/test/java/cl/mineduc/sidep/parvuloapi/ParvuloapiApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.mineduc.sidep.parvuloapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParvuloapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
